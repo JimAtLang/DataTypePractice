09f0e550d613a841d52fa51bbf7d417298db1b27
jimatlang <jim.stewart@thelangschool.org>
2023-09-18 10:07:42 -0400

    setup for students

--- Main.java
+++ Main.java
@@ -3,15 +3,6 @@ public class Main {
         int x = 5; 		// x is an integer
         int y = 2; 		// y is an integer
         double z = 2.0; 		// z is a double
-        System.out.println (x + y);	
-        System.out.println (x * z);
-        System.out.println (z - y);
-        System.out.println (x / y);
-        System.out.println (x / z);
-        System.out.println (1 == 1);
-        System.out.println (2 + 2 == 5);
-        System.out.println (true && false);
-        System.out.println (!true);
         
     }
 }
