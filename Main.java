f3b8d2e847442efdf3515481aee3cbe87ec4afd6
jimatlang <jim.stewart@thelangschool.org>
2023-09-18 10:05:53 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        int x = 5; 		// x is an integer
+        int y = 2; 		// y is an integer
+        double z = 2.0; 		// z is a double
+        System.out.println (x + y);	
+        System.out.println (x * z);
+        System.out.println (z - y);
+        System.out.println (x / y);
+        System.out.println (x / z);
+        System.out.println (1 == 1);
+        System.out.println (2 + 2 == 5);
+        System.out.println (true && false);
+        System.out.println (!true);
+        
+    }
+}
